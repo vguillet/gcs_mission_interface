f0d85939211878dbfa537f086529bf07ada3386a
Victor Guillet <victor.guillet@gmail.com>
2024-09-27 17:28:57 +0200

    update

--- gcs_mission_interface/gcs_mission_interface.py
+++ gcs_mission_interface/gcs_mission_interface.py
@@ -32,15 +32,27 @@ from rclpy.qos import QoSProfile, QoSReliabilityPolicy, QoSHistoryPolicy
 # Local Imports
 from orchestra_config.orchestra_config import *     # KEEP THIS LINE, DO NOT REMOVE
 
-from maaf_tools.datastructures.task.Task import Task
-from maaf_tools.datastructures.task.TaskLog import TaskLog
+try:
+    from maaf_tools.datastructures.task.Task import Task
+    from maaf_tools.datastructures.task.TaskLog import TaskLog
 
-from maaf_tools.datastructures.agent.Agent import Agent
-from maaf_tools.datastructures.agent.Fleet import Fleet
-from maaf_tools.datastructures.agent.AgentState import AgentState
-from maaf_tools.datastructures.agent.Plan import Plan
+    from maaf_tools.datastructures.agent.Agent import Agent
+    from maaf_tools.datastructures.agent.Fleet import Fleet
+    from maaf_tools.datastructures.agent.AgentState import AgentState
+    from maaf_tools.datastructures.agent.Plan import Plan
 
-from maaf_tools.tools import *
+    from maaf_tools.tools import *
+
+except ImportError:
+    from maaf_tools.maaf_tools.datastructures.task.Task import Task
+    from maaf_tools.maaf_tools.datastructures.task.TaskLog import TaskLog
+
+    from maaf_tools.maaf_tools.datastructures.agent.Agent import Agent
+    from maaf_tools.maaf_tools.datastructures.agent.Fleet import Fleet
+    from maaf_tools.maaf_tools.datastructures.agent.AgentState import AgentState
+    from maaf_tools.maaf_tools.datastructures.agent.Plan import Plan
+
+    from maaf_tools.maaf_tools.tools import *
 
 from .gcs_maaf_node import MAAFNode
 from .ui_singleton import UiSingleton
